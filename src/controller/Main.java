34b46cac77f30b5f10c865da40e64f032781711c
Gumballinthegetto <93859072+Gumballinthegetto@users.noreply.github.com>
2022-04-13 20:00:01 +0700

    add application icon

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -3,6 +3,7 @@ package controller;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,9 +12,13 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainForm.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 640);
         stage.setTitle("Akara Podcast");
         stage.setScene(scene);
+
+        Image applicationIcon = new Image(getClass().getResourceAsStream("/image/Logo.png"));
+        stage.getIcons().add(applicationIcon);
+
         stage.show();
     }
 
--- src/controller/MainFormController.java
+++ src/controller/MainFormController.java
@@ -1,5 +1,59 @@
 package controller;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 
-public class MainFormController {
+import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
+import java.util.ResourceBundle;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 
+public class MainFormController implements Initializable {
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    @FXML
+    private BorderPane borderPane;
+
+    @FXML
+    private void communityClicked(MouseEvent event) throws IOException {
+
+        VBox community = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/view/Community.fxml")));
+        borderPane.setCenter(community);
+    }
+
+    @FXML
+    private void discoverClick(MouseEvent event) throws IOException {
+
+        VBox discover = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/view/Discover.fxml")));
+        borderPane.setCenter(discover);
+    }
+
+    @FXML
+    private void favoriteClick(MouseEvent event) throws IOException {
+
+        VBox favorite = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/view/Favorite.fxml")));
+        borderPane.setCenter(favorite);
+    }
+
+    @FXML
+    private void playlistClick(MouseEvent event) throws IOException {
+
+        VBox playlist = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/view/Playlist.fxml")));
+        borderPane.setCenter(playlist);
+    }
+
+    @FXML
+    private void trendingClicked(MouseEvent event) throws IOException {
+
+        VBox trending = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/view/Trending.fxml")));
+        borderPane.setCenter(trending);
+    }
 }
