2863a49f939998776aaf871e51b7208750443455
Nuth Vireak <timberr.4t@gmail.com>
2022-08-04 18:09:36 +0700

    Updated ChannelController and Channel.fxml clear comment in MainFormController Add new 2 file css and new image

--- src/controller/ChannelController.java
+++ src/controller/ChannelController.java
@@ -1,5 +1,22 @@
 package controller;
 
-public class ChannelController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
+import model.Podcast;
 
+import java.net.URL;
+import java.util.Objects;
+import java.util.ResourceBundle;
+
+public class ChannelController implements Initializable {
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
