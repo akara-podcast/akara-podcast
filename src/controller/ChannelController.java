2380114c5187e2782861dd584db168fe1feb3bc3
Nuth Vireak <timberr.4t@gmail.com>
2022-08-13 10:18:31 +0700

    Enhancement MainForm and Discover and channel UI new added podcastHboxLongPodcaster.fxml and PodcastHboxLongPodcasterController

--- src/controller/ChannelController.java
+++ src/controller/ChannelController.java
@@ -1,22 +1,93 @@
+/*-----------------------------------------------------------------------------------------
+ * NAME : ChannelController.java
+ * VER  : v0.1
+ * PROJ : Akara
+ * CODE CLEAN? : No
+ *-----------------------------------------------------------------------------------------
+ *                      H      I      S      T      O      R      Y
+ *-----------------------------------------------------------------------------------------
+ *   DATE        AUTHOR         DESCRIPTION
+ * ----------  --------------  ------------------------------------------------------------
+ * 2022-08-13   Nuth Vireak     creation
+ * ----------  --------------  ------------------------------------------------------------
+ * 2022-08-13   Nuth Vireak     Modification
+ *---------------------------------------------------------------------------------------*/
+
 package controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Circle;
 import model.Podcast;
+import podcastData.DataInitializer;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class ChannelController implements Initializable {
 
+    //------------------------------------------------------------------------------------
+    // fields declaration                                                               |
+    //------------------------------------------------------------------------------------
+
+    @FXML
+    private HBox popularReleaseContainer;
+
+    @FXML
+    private VBox podcastContainer;
+
+    List<Podcast> popularPodcast;
+
+    //------------------------------------------------------------------------------------
+    //  Methods declaration                                                              |
+    //------------------------------------------------------------------------------------
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+        popularPodcast = new ArrayList<>(getPopularPodcast());
+
+        try {
+            for (Podcast podcast : popularPodcast) {
+                AnchorPane anchorPane = getAnchorPane(podcast);
+                popularReleaseContainer.getChildren().add(anchorPane);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static List<Podcast> getPopularPodcast() {
+
+        DataInitializer dataInitializer = new DataInitializer();
+        List<Podcast> popularPodcast = new ArrayList<>();
+
+        for (Podcast podcast : dataInitializer.podcastList()) {
+            if (podcast.getViewCount() > 5000) {
+                if (popularPodcast.size() < 10) {
+                    popularPodcast.add(podcast);
+                }}}
+        return popularPodcast;
+    }
+
+    private AnchorPane getAnchorPane(Podcast podcast) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("/view/podcastVbox.fxml"));
+
+        AnchorPane anchorPane = fxmlLoader.load();
+        PodcastVboxController podcastVboxController = fxmlLoader.getController();
+        podcastVboxController.setData(podcast);
+        return anchorPane;
     }
 }
