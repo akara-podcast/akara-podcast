bd5004f7f464913f2cc240894928803f549c1f35
nuth-vireak <timberr.4t@gmail.com>
2023-01-28 22:46:46 +0700

    Fixed Favorite function

--- src/controller/FavoriteController.java
+++ src/controller/FavoriteController.java
@@ -7,6 +7,7 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -21,20 +22,28 @@ public class FavoriteController implements Initializable {
 
     private HBox hBox;
 
-    private static FavoriteController instance = new FavoriteController(); //
-
-    private FavoriteController(){} //
+    private static final FavoriteController instance = new FavoriteController();
+    public static VBox favoriteContainerStatic;
+    public static String titleMediaPlayerTextStatic;
+    public static String podcasterMediaPlayerTextStatic;
+    // TODO: Add static field of Gnere, Duration
 
     public static FavoriteController getInstance() {
         return instance;
     }
 
-    public static VBox favoriteContainerStatic;
+    public FavoriteController(){
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
         favoriteContainerStatic = favoriteContainer;
+        titleMediaPlayerTextStatic = MediaPlayerController.titleMediaPlayerStatic.getText();
+        podcasterMediaPlayerTextStatic = MediaPlayerController.podcasterMediaPlayerStatic.getText();
+        // TODO: Initialize static field of Gnere, Duration from MediaPlayerController
+        System.out.println(titleMediaPlayerTextStatic);
+        System.out.println(podcasterMediaPlayerTextStatic);
+
     }
 
     @FXML
@@ -46,5 +55,18 @@ public class FavoriteController implements Initializable {
         hBox = fxmlLoader.load();
 
         favoriteContainerStatic.getChildren().add(hBox);
+    } // Test
+
+    public void addPodcastToFavorite() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(FavoriteController.class.getResource("/view/podcastHboxLong.fxml"));
+        hBox = fxmlLoader.load();
+        favoriteContainerStatic.getChildren().add(hBox);
+        System.out.println(titleMediaPlayerTextStatic);
+
+        //set hello to the text of the label in the podcastHboxLong.fxml
+        PodcastHboxLongController.titleHboxLongStatic.setText(titleMediaPlayerTextStatic);
+        PodcastHboxLongController.podcasterHboxLongStatic.setText(podcasterMediaPlayerTextStatic);
+        // TODO: Set text of Genre, Duration to PodcastHboxLongController
     }
 }
