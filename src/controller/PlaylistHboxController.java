92f754dabc6d824ff2ee961e3eea2c1f22ef6460
Tan Bunnchhay <bunchay1280@gmail.com>
2023-02-02 09:52:55 +0700

    Create AddPlaylistDialog fxml and Controller and PlaylistHbox fxml and controller. I will make it sync after

---
+++ src/controller/PlaylistHboxController.java
@@ -0,0 +1,14 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class PlaylistHboxController {
+    @FXML
+    private ImageView playlistIMG;
+
+    @FXML
+    private Label playlistName;
+
+}
